794d490fb3b8a904f0aba317e7f084fb9a49c391
Jshowker <167625031+jshowker@users.noreply.github.com>
2025-01-27 19:46:57 +0100

    Update main.mjs

--- main-process/main.mjs
+++ main-process/main.mjs
@@ -1,8 +1,7 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
-import { fileURLToPath } from "url"; // Убедитесь, что функция импортируется правильно
+import { fileURLToPath } from "url"; 
 
-// Создаём эквивалент __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let mainWindow;
@@ -26,7 +25,6 @@ function createWindow() {
         mainWindow = null;
     });
 
-    // Отправляем состояние окна (максимизировано/восстановлено) в рендерер
     mainWindow.on("maximize", () => {
         mainWindow.webContents.send("window-maximize-state-changed", true);
     });
@@ -35,7 +33,6 @@ function createWindow() {
     });
 }
 
-// IPC обработчики для управления окном
 ipcMain.on("window-minimize", () => {
     if (mainWindow) mainWindow.minimize();
 });
@@ -52,7 +49,6 @@ ipcMain.on("window-close", () => {
     if (mainWindow) mainWindow.close();
 });
 
-// Запуск приложения
 app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
